211d90943293f0756c76eb9561e5e90138d27302
soyfriss <federico.rissone@gmail.com>
2023-01-15 00:53:21 -0300

    add Landing Page tests

---
+++ client/src/tests/LandingPage.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
+import LandingPage from "../components/LandingPage.jsx";
+
+describe('Landing Page', () => {
+    it("should contain the logo text", () => {
+        render(
+            <BrowserRouter>
+                <LandingPage />
+            </BrowserRouter>
+        );
+        expect(screen.getAllByText(/BREED EXPLORER/)).toBeDefined();
+    });
+
+    it("should contain cta buttons", () => {
+        render(
+            <BrowserRouter>
+                <LandingPage />
+            </BrowserRouter>
+        );
+        expect(screen.getAllByRole("button", { name: "Try it free" })).toBeDefined();
+    });
+})
