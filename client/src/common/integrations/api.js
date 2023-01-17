c5c2f896c0d91179fe24039a609d9e36ed8e3691
soyfriss <federico.rissone@gmail.com>
2023-01-17 08:59:01 -0300

    change api url

--- client/src/common/integrations/api.js
+++ client/src/common/integrations/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as errors from '../../common/constants/errors.js';
 import * as httpStatusCodes from '../../common/constants/httpStatusCodes.js';
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "http://10.1.20.20:3001";
 axios.defaults.timeout = 15000;
 
 export async function getBreeds(searchText, exactSearch = false) {
