cd3e1591a4383e622cf13caaddfb94d8cc87165c
soyfriss <federico.rissone@gmail.com>
2023-01-21 00:02:07 -0300

    change api cors settings

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-dogs-frontend.onrender.com');
+  res.header('Access-Control-Allow-Origin', 'https://bx.onrender.com');
   // res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Credentials', 'true');
